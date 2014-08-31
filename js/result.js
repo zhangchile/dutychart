980d0b28e2fe0d20e2fbb8ab693721fecafe9826
zhangchile <zzlchile@gmail.com>
2014-09-01 00:05:31 +0800

    已经完成两个表的统计~

--- js/result.js
+++ js/result.js
@@ -11,23 +11,26 @@ var start_time = [];
 var end_time = [];
 
 var total_duration = 0;
+var total_real = userdata.length;
 var max = 0;
 var min = 0;
 var earliest = 0;//最早到
 var latest = 0;//最晚走
 
 var toTime = function(time) {
-    var date = new Date("2014/11/11 " + time);
-    return date.getTime();
+    var date =  date;
+    var datetime = new Date("2014/11/11 " + time);
+    return datetime.getTime();
 }
 
-for(var i=0,n=userdata.length; i < n; i++) {
+for(var i=0,n = userdata.length; i < n; i++) {
     date.push(userdata[i].date);
     weekdate.push(userdata[i].weekdate);
     duration.push(parseFloat(userdata[i].duration));
     start_time.push(userdata[i].start_time);
     end_time.push(userdata[i].end_time);
 
+    if(userdata[i].duration == 0) total_real -= 1;
     total_duration += parseFloat(userdata[i].duration);
     //求最大最小值工作时间的index
     if ( parseFloat(userdata[i].duration) > parseFloat(userdata[max].duration) ) {
@@ -47,15 +50,17 @@ for(var i=0,n=userdata.length; i < n; i++) {
 }
 
 //统计数据
+var avg_duration = parseFloat(total_duration / total_real).toFixed(2);
+var avg_dru_arr = [];
+for(var i=0,n=userdata.length; i < n; i++) { 
+    avg_dru_arr.push(parseFloat(avg_duration));
+}
+// console.log(avg_dru_arr);
 
-var avg_duration = parseFloat(total_duration / userdata.length).toFixed(2);
-
-
-$(function () { 
-    $('#container').highcharts({
-        chart: {
-            type: 'bar'
-        },
+//渲染图表配置
+$(document).ready(function(){
+$(function () {
+ $('#container').highcharts({
         title: {
             text: '工作时长'
         },
@@ -63,13 +68,82 @@ $(function () {
             categories: date
         },
         yAxis: {
+            min: 8,
+            tickInterval:0.5,
             title: {
-                text: '时间（小时）'
+                text: '时长（小时）'
             }
         },
         series: [{
+            type: 'column',
             name: '你',
             data: duration
+        }, {
+            type: 'spline',
+            name: 'Average',
+            data: avg_dru_arr,
+            marker: {
+                lineWidth: 2,
+                lineColor: Highcharts.getOptions().colors[3],
+                fillColor: 'white'
+            }
         }]
     });
 });
+
+$(function () {
+
+    var data2 = [];
+    for (var i = userdata.length - 1; i >= 0; i--) {
+        //把时间换成点
+        data2[i] = [new Date(date[i]).getTime(),
+                    isNaN(parseFloat(start_time[i].substring(0,5).replace(':','.')))  ? null : parseFloat(start_time[i].substring(0,5).replace(':','.')), 
+                    isNaN(parseFloat(end_time[i].substring(0,5).replace(':','.'))) ? null : parseFloat(end_time[i].substring(0,5).replace(':','.'))];
+
+    };
+
+// console.log(data2);
+    $('#container2').highcharts({
+    
+        chart: {
+            type: 'arearange'
+        },
+        
+        title: {
+            text: '工作时间'
+        },
+    
+        xAxis: {
+            type: 'datetime',
+            tickInterval: 24*3600*1000
+        },
+        
+        yAxis: {
+            reversed: true,
+            tickInterval : 0.5,
+            title: {
+                text: '时间'
+            }
+        },
+
+        tooltip: {
+            crosshairs: true,
+            shared: true,
+        },
+    
+        series: [{
+            name: '时间',
+            data: data2,
+            dataLabels: {
+                enabled: true,
+                yHigh: 20,
+                yLow: -20
+            }
+        }]
+    
+    });
+});
+
+
+
+});//onload end
